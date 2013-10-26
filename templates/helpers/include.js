257c2cbde40950638ecc55af20d5b4bd1b171e8f
Jon Schlinkert <dev@sellside.com>
2013-10-26 06:27:46 -0400

    rename to include

---
+++ templates/helpers/include.js
@@ -0,0 +1,51 @@
+/**
+ * Handlebars Helpers: {{pager}}
+ * Copyright (c) 2013 Jon Schlinkert
+ * Licensed under the MIT License (MIT).
+ */
+
+'use strict';
+
+
+// Node.js
+var path = require('path');
+var fs   = require('fs');
+
+
+
+// Export helpers
+module.exports.register = function (Handlebars, options, params) {
+
+  var opts     = options;
+  var grunt    = params.grunt;
+  var assemble = params.assemble;
+  var _        = require('lodash');
+
+  var configData = grunt.config.process(grunt.config.data);
+  var data = grunt.file.expand(configData.assemble.options.data);
+
+  var localContext = data.map(function(src) {
+    return _.extend({}, configData, src);
+  });
+
+  // console.log(localContext);
+
+  /**
+   * {{include}}
+   */
+  exports.include = function(template, context) {
+    template = Handlebars.compile(template);
+    context = _.extend({}, this, localContext, opts);
+    return new Handlebars.SafeString(template(context));
+  };
+
+
+  for (var helper in exports) {
+    if (exports.hasOwnProperty(helper)) {
+      Handlebars.registerHelper(helper, exports[helper]);
+    }
+  }
+};
+
+
+
